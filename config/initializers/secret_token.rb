475e359a90047c7309257c1f1132b81ed9c7d0f4
Creighton Higgins <chiggins@chiggins.com>
2014-08-18 12:16:46 -0700

    BikeTaDoo! initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Biketadoo::Application.config.secret_key_base = 'e41072ab59b32c7c85fff5c571362f02180056a01d46c1d8eee864b18007b75d7447df0eb8d70deab9db0e694983f8e0f500d087cdf2aed8e58eb1de2e0660c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Biketadoo::Application.config.session_store :cookie_store, key: '_biketadoo_session'
